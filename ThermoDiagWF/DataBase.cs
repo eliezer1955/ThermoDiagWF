44bf10642a7d913868cccc21972e9d6e9f133e48
eliezer1955 <eliezerr@s2genomics.com>
2024-03-27 07:43:33 -0700

    WIP

---
+++ ThermoDiagWF/DataBase.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.Sql;
+using System.Data.SqlClient;
+
+namespace ThermoDiagWF
+{
+    internal class DataBase
+    {
+        public  SqlConnection db;
+       
+        public DataBase()
+        {
+            db = new SqlConnection( "S2Bringup.sqlite3" );
+            db.Open();
+
+        }
+
+
+        public void  Open() 
+        {
+            db.Open();
+        }
+        public void Close() 
+        {
+            db.Close();
+        }
+
+
+
+    }
+}
